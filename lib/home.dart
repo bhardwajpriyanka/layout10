abb8e1a4f7745ae08efb023b4b1e2903693ad70e
priyanka <bharadwajpriyanka204@gmail.com>
2023-02-21 20:48:51 -0800

    layout10

---
+++ lib/home.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+class home extends StatefulWidget {
+  const home({Key? key}) : super(key: key);
+
+  @override
+  State<home> createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Container(
+                  height: 100,
+                  width: 100,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.white, width: 2)),
+                  child: Icon(
+                    Icons.restaurant_menu_outlined,
+                    color: Colors.white,
+                    size: 50,
+                  )),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.person, color: Colors.white),
+                SizedBox(
+                  width: 20,
+                ),
+                Container(
+                  height: 35,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                  child: Text("Enter Email",
+                      style: TextStyle(color: Colors.white, letterSpacing: 0)),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.lock_outline_rounded, color: Colors.white),
+                SizedBox(
+                  width: 20,
+                ),
+                Container(
+                  height: 35,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                  child: Text("Enter Password",
+                      style: TextStyle(color: Colors.white, letterSpacing: 0)),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Container(
+              alignment: Alignment.center,
+              height: 40,
+              width: 200,
+              decoration: BoxDecoration(
+                color: Colors.pink,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(50),
+                ),
+              ),
+              child: Text("Sign In", style: TextStyle(color: Colors.white,letterSpacing: 2),),
+            ),
+            SizedBox(height: 50,),
+            Text("Don't have an account? Sign Up",style: TextStyle(color: Colors.white),),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:layout10/home.dart';
+
+void main()
+{
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: home(),
+      )
+  );
+}
